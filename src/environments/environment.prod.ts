1cad0e97801cdc2b4d599cf07e5df1b8553e4143
Andre Noberto <andrenoberto@protonmail.com>
2021-07-22 00:30:39 +0100

    Updated prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'http://localhost:3000/api/v1',
+  apiUrl: 'http://ac1e4db1cd3be4204b699aa06de9dd2e-1332902847.us-east-1.elb.amazonaws.com:3000/api/v1',
   production: true,
 };
