189481d8bee02e32d8b3b39a548859b85f331264
Andre Noberto <andrenoberto@protonmail.com>
2021-07-21 23:18:17 +0100

    Fixed lint errors and updated breaking tests

--- src/app/modules/home/components/social-button/social-button.component.spec.ts
+++ src/app/modules/home/components/social-button/social-button.component.spec.ts
@@ -9,7 +9,6 @@ describe('SocialButtonComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [ FontAwesomeModule ],
       declarations: [ SocialButtonComponent ],
     })
     .compileComponents();
